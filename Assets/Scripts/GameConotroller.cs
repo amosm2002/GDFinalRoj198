ac97fac8c52c53d3a9233ad7c7cf1964c3024122
Amos Munteanu <amosmunteanu@gmail.com>
2023-11-19 02:17:10 -0600

    Created Start Menu and Tutorial Scences

---
+++ Assets/Scripts/GameConotroller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+}
