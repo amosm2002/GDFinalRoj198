3fc488aa1012944ce12a1c1c9e59f80f21739a7e
Amos Munteanu <amosmunteanu@gmail.com>
2023-11-20 11:57:22 -0600

    Added coins to collect and added a finish level object

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public Text scoreText;
+    private int score = 0;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + score;
+        }
+    }
+}
